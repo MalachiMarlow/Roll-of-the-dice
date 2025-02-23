aaa231e64b5a12a98c8437d7ff628989086badba
Tusk <Tuskman045@gmail.com>
2025-02-23 12:48:14 -0700

    Add project files.

---
+++ Roll of the dice/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Roll of the dice")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Idaho State University")>
+<Assembly: AssemblyProduct("Roll of the dice")>
+<Assembly: AssemblyCopyright("Copyright © Idaho State University 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0137211d-6ef8-4b05-bb99-6c93e01d741b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Roll of the dice/Roll_Of_The_Dice.vb
@@ -0,0 +1,45 @@
+﻿Module Roll_Of_The_Dice
+
+    Sub Main()
+
+    End Sub
+
+    Function diceTracker(dieRoll As Integer, die As Integer) As Boolean(,)
+        Static _diceTracker(14, 4) As Boolean
+        'actual code here
+        Return _diceTracker
+
+    End Function
+
+    Sub randoDice()
+        Dim temp As String = "X |"
+        Dim heading() As String = {"Roll of the dice"}
+        For Each letter In heading
+            Console.Write(letter.PadLeft(2).PadRight(4))
+        Next
+        Console.WriteLine()
+        Console.WriteLine(StrDup(20, "_"))
+        For i = 1 To 15
+            For j = 1 To 5
+                temp = temp.PadLeft(4)
+                Console.Write(temp)
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+
+    Function randomNumberBetween(max As Integer, min As Integer) As Integer
+        Dim temp As Single
+
+        Randomize()
+        temp = Rnd()
+        'establish range
+        temp *= (max + 1) - min 'makes range bigger by 1 to fix inclusivity max/min
+        temp += min - 1 'shifts range down so math.ceiling works
+        'Return CInt(temp) 'bad randomness
+        'Return CInt(Int(temp)) 'randomness ok but max is not included
+        Return CInt(Math.Floor(temp)) 'randomness ok but max is not included
+        'Return CInt(Math.Ceiling(temp)) 'randomness ok but min is not included
+    End Function
+
+End Module
